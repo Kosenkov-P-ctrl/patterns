1b17039fb854f0df95c20b83af6719b6976727b7
Kosenkov Pavel <pkosenkov@5team.tech>
2024-02-01 17:05:53 +0700

    P-6:add:singleton

--- src/main/java/org/patterns/singlton/ChocolateBoiler.java
+++ src/main/java/org/patterns/singlton/ChocolateBoiler.java
@@ -1,4 +1,40 @@
 package org.patterns.singlton;
 
 public class ChocolateBoiler {
+    private boolean empty;
+    private boolean boiled;
+    private static ChocolateBoiler chocolateBoiler;
+    private ChocolateBoiler(){
+        this.empty = true;
+        this.boiled = false;
+    }
+    public static ChocolateBoiler getInstance(){
+        if(chocolateBoiler == null){
+            chocolateBoiler = new ChocolateBoiler();
+        }
+        return chocolateBoiler;
+    }
+
+    public void fill() {
+        if (isEmpty()) {
+            empty = false;
+            boiled = false;
+        }
+    }
+    public void drain() {
+        if (!isEmpty() && isBoiled()) {
+            empty = true;
+        }
+    }
+    public void boil() {
+        if (!isEmpty() && !isBoiled()) {
+            boiled = true;
+        }
+    }
+    public boolean isEmpty() {
+        return empty;
+    }
+    public boolean isBoiled() {
+        return boiled;
+    }
 }
--- src/main/java/org/patterns/singlton/Singleton.java
+++ src/main/java/org/patterns/singlton/Singleton.java
@@ -1,5 +1,14 @@
 package org.patterns.singlton;
 
 public class Singleton {
+    private static Singleton singleton;
 
+    private Singleton(){}
+
+    public static Singleton getInstance(){
+        if(singleton == null){
+            singleton = new Singleton();
+        }
+        return singleton;
+    }
 }
